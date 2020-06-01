28c5f8277fab5b8da1b0812ab536060347520e31
Leroycodes <leroysnee@gmail.com>
2020-06-01 20:35:21 +1200

    playground

---
+++ index.js
@@ -0,0 +1,11 @@
+ function namePrinter(firstName, lastName) {
+     return firstName + ' ' + lastName;
+ }
+
+ function addParam(param1, param2, param3) {
+     return param1 + param2 + param3
+ }
+
+ const result = addParam(1, 2, 3);
+
+ console.log('result ' + result)
