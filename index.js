35d3c0e2d67145558af5c70cddaa3a97701268a5
Leroycodes <leroysnee@gmail.com>
2020-06-09 19:26:01 +1200

    if condition

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
- function namePrinter(firstName, lastName) {
-     return firstName + ' ' + lastName;
- }
+//  function namePrinter(firstName, lastName) {
+//      return firstName + ' ' + lastName;
+//  }
 
- function addParam(param1, param2, param3) {
-     return param1 + param2 + param3
- }
+//  function addParam(param1, param2, param3) {
+//      return param1 + param2 + param3
+//  }
 
- const result = addParam(1, 2, 3);
+//  const result = addParam(1, 2, 3);
 
- console.log('result ' + result)
+//  console.log('result ' + result)
+
+function nameGame(firstname, lastname) {
+  if (lastname === "grimmer") {
+    return "seth";
+  } else if (firstname === "leroy") {
+    return "grimmer"
+  }
+}
+
+const name = nameGame("leroy", "grimmer");
+
+console.log(name);
