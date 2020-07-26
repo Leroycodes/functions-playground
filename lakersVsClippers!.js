25861196407f285de74af2f214f3b475421769d1
Leroycodes <leroysnee@gmail.com>
2020-07-26 20:18:53 +1200

    Gamescore function

---
+++ lakersVsClippers!.js
@@ -0,0 +1,9 @@
+function gameScore(lakersScore, clippersScore) {
+    if (lakersScore > clippersScore) {
+        console.log('Lakers win! ' + lakersScore)
+    } else {
+        console.log('Clippers win! ' + clippersScore)
+    }
+}
+
+gameScore(1, 66);
